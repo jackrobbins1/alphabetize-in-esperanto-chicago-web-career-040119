4bd94c87b7950e1917b70225ac61ffc9e50af24e
Jack Robbins <jackrobbins1@gmail.com>
2019-03-28 16:03:24 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
-def alphabetize(arr)
-  # code here
+require "pry"
+
+def esperan
+  "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 end
+
+def alphabetize(arr)
+  arr.sort{|s1, s2| s1.cmp_loc(s2, alphabet)}
+end
+
+binding.pry
